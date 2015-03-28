e0fb5d81accd0f386f321c59b4694aebbed3b87c
Boyan Todorov <Boyan.digital@Gmail.com>
2015-03-28 14:13:53 +0200

    Python Homeworks

--- Programming0-1/week7/5-Sandclock/sands_of_time.py
+++ Programming0-1/week7/5-Sandclock/sands_of_time.py
@@ -1,1 +1,19 @@
 __author__ = 'Боян'
+
+num = input("Enter a number: ")
+n = int(num)
+dot = "."
+asterisk = "*"
+clock = ""
+# upper part
+for x in range(0, n // 2 + 1):
+    dots = x
+    asterisks = n - 2 * dots
+    clock += dot * dots + asterisk * asterisks + dot * dots + "\n"
+# lower part
+for x in range(n // 2 - 1, -1, -1):
+    dots = x
+    asterisks = n - 2 * dots
+    clock += dot * dots + asterisk * asterisks + dot * dots + "\n"
+
+print(clock)
