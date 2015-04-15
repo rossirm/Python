b2f38567f58ea20a0d5bb02bb06297b8e2db0af1
Boyan Todorov <Boyan.digital@Gmail.com>
2015-04-15 08:04:11 +0300

    Python Exam

--- Programming0/exam/1-Loss-or-Profit/loss_or_profit.py
+++ Programming0/exam/1-Loss-or-Profit/loss_or_profit.py
@@ -7,17 +7,12 @@ def loss_or_profit(income, outcome):
 
     total = income_sum - loss_sum
 
-    if total > 0:
-        print("+{0}".format(total))
-    elif total == 0:
-        print("={0}".format(total))
-    else:
-        print("{0}".format(total))
+    return total
 
 
-loss_or_profit([1, 2, 3], [3])
+print(loss_or_profit([1, 2, 3], [3]))
 # '+3'
-loss_or_profit([10], [20, 30])
+print(loss_or_profit([10], [20, 30]))
 # '-40'
-loss_or_profit([10], [10])
+print(loss_or_profit([10], [10]))
 # '=0'
