324339ba2749b00343c45329ee92211f2ec4bdc6
BoyanTodorov <boyan.digital@gmail.com>
2017-06-12 08:21:28 +0300

    Simple Loops [Homework]

--- SoftUni/Python-Basics/Simple-Loops/odd_even_position.py
+++ SoftUni/Python-Basics/Simple-Loops/odd_even_position.py
@@ -5,10 +5,10 @@ import sys
 
 count = int(input())
 
-odd_sum = 0
+odd_sum = 0.0
 odd_minimal = sys.maxsize
 odd_maximal = -sys.maxsize
-even_sum = 0
+even_sum = 0.0
 even_minimal = sys.maxsize
 even_maximal = -sys.maxsize
 
@@ -29,21 +29,21 @@ for number in range(count):
 
 result = ''
 if count == 0:
-    result = 'OddSum={0:.1f}, OddMin=No, OddMax=No, ' \
-             'EvenSum={1:.1f}, EvenMin=No, EvenMax=No'.format(odd_sum, even_sum)
+    result = 'OddSum={0}, OddMin=No, OddMax=No, ' \
+             'EvenSum={1}, EvenMin=No, EvenMax=No'.format(odd_sum, even_sum)
 elif count == 1:
-    result = 'OddSum={0:.1f}, OddMin={1:.1f}, OddMax={2:.1f}, ' \
-             'EvenSum={3:.1f}, EvenMin=No, EvenMax=No'.format(round(odd_sum, 2),
-                                                              round(odd_minimal, 2),
-                                                              round(odd_maximal, 2),
-                                                              round(even_sum, 2))
+    result = 'OddSum={0}, OddMin={1}, OddMax={2}, ' \
+             'EvenSum={3}, EvenMin=No, EvenMax=No'.format(odd_sum,
+                                                          odd_minimal,
+                                                          odd_maximal,
+                                                          even_sum)
 else:
-    result = 'OddSum={0:.1f}, OddMin={1:.1f}, OddMax={2:.1f}, ' \
-             'EvenSum={3:.1f}, EvenMin={4:.1f}, EvenMax={5:.1f}'.format(round(odd_sum, 2),
-                                                                        round(odd_minimal, 2),
-                                                                        round(odd_maximal, 2),
-                                                                        round(even_sum, 2),
-                                                                        round(even_minimal, 2),
-                                                                        round(even_maximal, 2))
+    result = 'OddSum={0}, OddMin={1}, OddMax={2}, ' \
+             'EvenSum={3}, EvenMin={4}, EvenMax={5}'.format(odd_sum,
+                                                            odd_minimal,
+                                                            odd_maximal,
+                                                            even_sum,
+                                                            even_minimal,
+                                                            even_maximal)
 
 print(result)
