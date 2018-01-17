ab53700a812b899479c022f0ab37f67f3565a7fe
BoyanTodorov <boyan.digital@gmail.com>
2018-01-17 11:54:25 +0200

    Python Intro

---
+++ Python-Fundamentals/Python-Intro/extended_person_info.py
@@ -0,0 +1,27 @@
+name = input()
+age = int(input())
+town = input()
+salary = float(input())
+
+
+def check_age(some_age):
+    if some_age < 18:
+        return 'teen'
+    elif some_age < 70:
+        return 'adult'
+    else:
+        return 'elder'
+
+
+def check_salary(some_salary):
+    if some_salary < 500:
+        return 'low'
+    elif some_salary < 2000:
+        return 'medium'
+    else:
+        return 'high'
+
+
+print(
+    f'Name: {name}\nAge: {age}\nTown: {town}\nSalary: ${salary:.2f}\n'
+    f'Age range: {check_age(age)}\nSalary range: {check_salary(salary)}')
